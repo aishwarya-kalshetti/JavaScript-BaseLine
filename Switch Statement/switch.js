41d6295f9ee4fd7d729b23b161be44ad544cf296
aishwarya-kalshetti <aishwarya.kalshetti@tekditechnologies.com>
2025-08-16 00:21:19 +0530

    Add files via upload

---
+++ Switch Statement/switch.js
@@ -0,0 +1,21 @@
+const exp=5;
+switch(exp){
+    case 1:
+        console.log("You are in case 1");
+        break;
+    case 2:
+        console.log("You are in case 2");
+        break;
+    case 3:
+        console.log("You are in case 3");
+        break;
+    case 4:
+        console.log("You are in case 4");
+        break;
+    case 5:
+        console.log("You are in case 5");
+        break;
+    default:
+        console.log("default case");
+        break;
+    }

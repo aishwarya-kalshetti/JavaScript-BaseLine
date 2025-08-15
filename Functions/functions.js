95c31fa9a68922f7d4ff01fc459c6ef379bc5674
aishwarya-kalshetti <aishwarya.kalshetti@tekditechnologies.com>
2025-08-16 00:19:33 +0530

    Add files via upload

---
+++ Functions/functions.js
@@ -0,0 +1,26 @@
+let a=prompt("Enter");
+let b=prompt("Enter");
+
+const sum=(a,b)=>{
+    console.log(a+b);
+};
+const mul=(a,b)=>{
+    console.log(a*b);
+};
+const div=(a,b)=>{
+    console.log(a/b);
+};
+const sub=(a,b)=>{
+    console.log(a-b);
+};
+const mod=(a,b)=>{
+    console.log(a%b);
+};
+const exp=(a,b)=>{
+    console.log(a**b);
+};
+
+const printHello=()=>{
+    console.log("Hello");
+};
+
